6a704aa41a5580431bda1d57221b56b1b26cc350
Sofia <mashkovtseva.sofia@lll.kpi.ua>
2025-05-19 21:32:28 +0300

    Change #1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+//Створимо базовий клас для всіх об'єктів
+abstract class BaseEntity
+{
+    public int ID { get; set; }
+
+    protected BaseEntity(int id)
+    {
+        ID = id;
+    }
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"ID: {ID}");
+    }
+}
