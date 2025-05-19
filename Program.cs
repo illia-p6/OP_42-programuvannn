2ef05590b7751fb3f3b54b2b8510917b31bdc944
Sofia <mashkovtseva.sofia@lll.kpi.ua>
2025-05-19 21:37:31 +0300

    Change #5

--- Program.cs
+++ Program.cs
@@ -40,3 +40,30 @@ class Admin : BaseEntity
         Password = password;
     }
 }
+
+class User : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    private string phone;
+
+    public string Phone
+    {
+        get => phone;
+        set => phone = value;
+    }
+
+    public User(int id, string firstName, string lastName, string email, string phone) : base(id)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Phone = phone;
+    }
+
+    public override void PrintInfo()
+    {
+        Console.WriteLine($"User: {FirstName} {LastName}, Email: {Email}");
+    }
+}
