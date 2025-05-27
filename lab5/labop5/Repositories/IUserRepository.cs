b31ec51f7a1108b38a58021e91275c0e0fb1549a
illia-p6 <illyakushnir28@gmail.com>
2025-05-27 22:58:40 +0300

    Додано ЛР5

---
+++ lab5/labop5/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using labop4.Models;
+
+
+namespace labop4.Repositories
+{
+   public interface IUserRepository : IRepository<UserReg>
+    {
+        UserReg GetByUsername(string username);
+
+    }
+}
---
+++ lab5/labop5/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using labop4.Models;
+
+namespace labop4.Repositories
+{
+    public class UserRepository : Repository<User>
+    {
+        public User GetByEmail(string email)
+        {
+            return _entities.FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
